7dab709897271726e24aaa950ba0ed6d10c2420a
siddharth dedhia <siddharthdedhia@yahoo.co.in>
2023-06-14 01:45:47 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Taxes made easy !
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:shiv.amgoth88@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React bro
+          Shiv Tax Berater
         </a>
       </header>
     </div>
